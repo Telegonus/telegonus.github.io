32cca991b8d62381b9d33a46c7da3c87102be4fe
Vegard Stürzinger <vegard.stuerzinger@gmail.com>
2024-03-21 20:00:56 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -6,13 +6,35 @@ document.addEventListener('DOMContentLoaded', function() {
         maxZoom: 18,
     }).addTo(map);
 
-    fetch('../coordinates.json')
-        .then(response => response.json())
-        .then(data => {
-            data.locations.forEach(location => {
-                L.marker([location.lat, location.lng]).addTo(map)
-                    .bindPopup(location.description);
-            });
+    fetch('../coordinates.json') // Make sure this path is correct
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
         })
-        .catch(error => console.log('Error loading coordinates:', error));
+        .then(data => {
+            // Initialize an array to hold the coordinates for the polyline
+            var polylineCoordinates = [];
+
+            data.locations.forEach(location => {
+                var latlng = [location.lat, location.lng];
+
+                // Add marker for each location
+                L.marker(latlng).addTo(map)
+                    .bindPopup(location.description);
+
+                // Add this location's coordinates to the array
+                polylineCoordinates.push(latlng);
+            });
+
+            // Create a polyline using the array of coordinates and add it to the map
+            var polyline = L.polyline(polylineCoordinates, {color: 'red'}).addTo(map);
+
+            // Optional: adjust the view to show all markers and polylines
+            map.fitBounds(polyline.getBounds());
+        })
+        .catch(error => {
+            console.error('Error loading coordinates:', error);
+        });
 });
